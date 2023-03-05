bdbf536a6a0ae453e9a3e2cb145dfce75963ec66
yumi <w0482166@nscc.ca>
2023-03-05 06:50:51 -0400

    3/5

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const socketIO = require('socket.io');
 const http = require('http');
+let numClient = 0;  //the number of current clients
 
 //create our express web app
 const app = express();
@@ -8,8 +9,7 @@ const app = express();
 //handle a request from a client (browser)
 app.get('/', (req, res) =>
 {
-    res.send("hello")
-    res.sendFile(`${__dirname/chat.html}`)
+    res.sendFile(`${__dirname}/chat.html`)
 });
 
 //prepare the express app to be used by socket IO
@@ -18,14 +18,38 @@ const httpServer = http.createServer(app).listen(3000);
 //create the chat server
 const io = socketIO(httpServer);
 
-//capturing the connection event...when a client connects
-io.on('connection', socket => {
-    console.log('client connected...!')
-    socket.emit('welcome', 'Welcome to the chat server!!')
+io.on('connection', (socket) => {
+        
+    socket.emit('welcome', 'Welcome from the Chat Server!!')
 
+    //increase the number of current clients
+    numClient++;
+        
+    //send the number of clients to all
+    io.emit('numberofclients', numClient);
+
+    //send current server-time to all
+    setInterval(() => {
+        const serverTime = new Date().toTimeString();
+        io.emit('server-time', serverTime)
+    }, 1000);
+
+    //handle incoming message from a client
+    socket.on('message', (message)=>{
+        //send this message to everyone
+        io.emit('message', message);
+    })           
+
+    //When disconnected
     socket.on('disconnect', ()=>
     {
-        console.log('Client Disconnected')
+        //decrease the number of current clients
+        numClient--;
+        //send the number of clients to all
+        io.emit('numberofclients', numClient);
+                
     })
-
 });
+
+
+
